0cd976a4cfd8bd13daad3ecf068f3c84543e595f
Adrian <adrianmiernik@gmail.com>
2023-11-03 20:08:53 +0100

    up

--- src/components/MainSite.js
+++ src/components/MainSite.js
@@ -1,21 +1,16 @@
-import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './NavBar';
 import BlockSite from './BlockSite';
-import Temp from './Temp';
 
 function MainSite() {
+
+
   return (
-    <Router>
-      <div className="MainSite">
-        <NavBar />
-        <Routes>
-          <Route path="/about-me/" element={<BlockSite />} />
-          <Route path="/temp" element={<Temp />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="MainSite">
+
+      <NavBar />
+      <BlockSite />
+    </div>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,24 +2,38 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
-import Temp from './Temp.js';
-import styles from './component-style.css';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import styles from './component-style.css'
+
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function NavBar() {
+
+
+
+
     return (
-        <Navbar expand="lg">
-            <Container>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link className="navbar-brand" as={Link} to="/about-me/"><b>Portfolio Site</b> <p>Miernik Adrian</p></Nav.Link>
-                        <Nav.Link className="main-link" as={Link} to="/temp">Temp</Nav.Link>
-                        <Nav.Link className="main-link" as={Link} to="/temp">Temp</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <Router>
+            <Navbar expand="lg" >
+                <Container>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link className="navbar-brand" as={Link} to="/about-me/"><b>Portfolio Site</b> <p>Miernik Adrian2</p></Nav.Link>
+                            <Nav.Link className="main-link" as={Link} to="/temp">Temp</Nav.Link>
+                            <Nav.Link className="main-link" as={Link} to="/temp">Temp</Nav.Link>
+
+                        </Nav>
+
+
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+            <Routes>
+
+            </Routes>
+        </Router>
     );
 }
 
