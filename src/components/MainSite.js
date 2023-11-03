286a5d04fd018a928186ef74aab9a663e473d10e
Adrian <adrianmiernik@gmail.com>
2023-11-03 20:04:26 +0100

    up

--- src/components/MainSite.js
+++ src/components/MainSite.js
@@ -1,16 +1,21 @@
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './NavBar';
 import BlockSite from './BlockSite';
+import Temp from './Temp';
 
 function MainSite() {
-
-
   return (
-    <div className="MainSite">
-
-      <NavBar />
-      <BlockSite />
-    </div>
+    <Router>
+      <div className="MainSite">
+        <NavBar />
+        <Routes>
+          <Route path="/about-me/" element={<BlockSite />} />
+          <Route path="/temp" element={<Temp />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Temp.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function Temp() {
+
+
+    return (
+        <div className="tmp">
+            Somday, something will be here
+        </div>
+    );
+}
+
+export default Temp;
